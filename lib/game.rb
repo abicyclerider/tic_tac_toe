5100b9cd8579d479d2d17ba77737bae78881b9d2
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-05-20 06:53:39 -0600

    Fix inconsistency in borad display after win

--- lib/game.rb
+++ lib/game.rb
@@ -14,6 +14,7 @@ class Game
   def play_round
     loop do
       if @player_x.take_turn?(@board, @player_o)
+        @board.display(@player_x, @player_o)
         break
       elsif @player_o.take_turn?(@board, @player_x)
         @board.display(@player_x, @player_o)
